e60ff9e90576ed4a393ab16959e399e3b67047f4
NithishaSathishkumar <nithisha132004@gmail.com>
2024-04-29 11:45:58 -0700

    Initial Commit

---
+++ Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Poor
+//
+//  Created by Nithisha Sathishkumar on 4/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
